b28fbae6784647fd852990e80eb467f1f75f64d8
jonathanbarzola <jonabl2102@gmail.com>
2022-10-24 23:56:53 -0500

    array students

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const students = [
+export const students = [
   {
     name: "Jill",
     lastname: "Doe",
@@ -30,22 +30,3 @@ const students = [
     course: "Financial Management",
   },
 ];
-
-/* 
-    Programación Imperativa
-
-    for (let i = 0; i < students.length; i++) {
-    console.log(students[i])
-    } 
-*/
-
-
-// Programación Declarativa
-
-students.forEach((student, index, stds) => {
-    console.log("Este es el elemento en la posición: " + index);
-    console.log(student.name + " "+ student.lastname);
-    console.log("\n");
-
-    // stds imprimiría el array students que es el que esta siendo recorrido actualmente
-})
